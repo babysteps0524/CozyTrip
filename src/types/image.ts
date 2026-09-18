629081493965e1dca09691f39a7711ce526172cb
babysteps0524 <babysteps0524@gmail.com>
2026-09-18 14:23:47 +0900

    feat: add MyRealTrip as hotel image source

--- src/types/image.ts
+++ src/types/image.ts
@@ -1,5 +1,5 @@
 export type ImageSource =
-  "rakuten" | "agoda" | "official" | "owned" | "licensed";
+  "rakuten" | "agoda" | "myrealtrip" | "official" | "owned" | "licensed";
 
 export type ImageType =
   | "hero"
