0f3a16fd21b2854c136f2ec62086fb4737b7fcc1
babysteps0524 <babysteps0524@gmail.com>
2026-09-19 09:58:16 +0900

    fix: type generated client hotel loaders

--- src/data/clientHotels.ts
+++ src/data/clientHotels.ts
@@ -16,12 +16,12 @@ const loaders: Record<
   DestinationSlug,
   () => Promise<ClientHotelFile>
 > = {
-  tokyo: () => import("./generated/client-hotels/tokyo.json"),
-  osaka: () => import("./generated/client-hotels/osaka.json"),
-  kyoto: () => import("./generated/client-hotels/kyoto.json"),
-  fukuoka: () => import("./generated/client-hotels/fukuoka.json"),
-  sapporo: () => import("./generated/client-hotels/sapporo.json"),
-  okinawa: () => import("./generated/client-hotels/okinawa.json"),
+  tokyo: async () => (await import("./generated/client-hotels/tokyo.json")) as unknown as ClientHotelFile,
+  osaka: async () => (await import("./generated/client-hotels/osaka.json")) as unknown as ClientHotelFile,
+  kyoto: async () => (await import("./generated/client-hotels/kyoto.json")) as unknown as ClientHotelFile,
+  fukuoka: async () => (await import("./generated/client-hotels/fukuoka.json")) as unknown as ClientHotelFile,
+  sapporo: async () => (await import("./generated/client-hotels/sapporo.json")) as unknown as ClientHotelFile,
+  okinawa: async () => (await import("./generated/client-hotels/okinawa.json")) as unknown as ClientHotelFile,
 };
 
 export async function loadClientHotels(
