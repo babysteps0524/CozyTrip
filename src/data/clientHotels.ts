be0a05d7a0f1f3c160c294c887cd9669281166dd
babysteps0524 <babysteps0524@gmail.com>
2026-09-20 23:18:44 +0900

    chore: remove unused destination client chunks

--- src/data/clientHotels.ts
+++ src/data/clientHotels.ts
@@ -10,10 +10,9 @@ interface ClientHotelModule {
 export type DestinationSlug =
   | "tokyo"
   | "osaka"
-  | "kyoto"
   | "fukuoka"
   | "sapporo"
-  | "okinawa";
+;
 
 const loaders: Record<
   DestinationSlug,
@@ -21,10 +20,8 @@ const loaders: Record<
 > = {
   tokyo: async () => (await import("./generated/client-hotels/tokyo.json")) as unknown as ClientHotelFile,
   osaka: async () => (await import("./generated/client-hotels/osaka.json")) as unknown as ClientHotelFile,
-  kyoto: async () => (await import("./generated/client-hotels/kyoto.json")) as unknown as ClientHotelFile,
   fukuoka: async () => (await import("./generated/client-hotels/fukuoka.json")) as unknown as ClientHotelFile,
   sapporo: async () => (await import("./generated/client-hotels/sapporo.json")) as unknown as ClientHotelFile,
-  okinawa: async () => (await import("./generated/client-hotels/okinawa.json")) as unknown as ClientHotelFile,
 };
 
 export async function loadClientHotels(
